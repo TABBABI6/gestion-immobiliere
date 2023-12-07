2cd6abb8f4371e45c9fe432824bf35b399d0ada3
TABBABI6 <mohamed.tabbabi@tek-up.de>
2023-12-07 12:11:59 +0100

    first commit

---
+++ src/test/java/com/example/gestionimobiliere/GestionImobiliereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionimobiliere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionImobiliereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
